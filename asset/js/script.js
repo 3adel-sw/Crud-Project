154ce8ec414a9b2217eb17eb8d2458ceac98d355
Adel Mahmoud <dolacalisthenicsw@gmail.com>
2025-04-20 15:26:23 +0200

    adding by code

---
+++ asset/js/script.js
@@ -0,0 +1,24 @@
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+
+console.log(title, price, taxes, ads, discount, total, count, category, submit);
+
+// Get Total
+function getTotal() {}
+
+// Create Product
+// Save Local Storage
+// Create Inputs
+// read
+// count
+// Delete
+// Update
+// Search
+// Clean data
